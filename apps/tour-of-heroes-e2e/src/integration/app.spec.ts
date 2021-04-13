3574a813fc0af117674ea9ff04ac7dde9e246c26
Moritz Eck <moritz.eck@zuehlke.com>
2021-04-13 16:25:04 +0200

    fix: cypress tests for new layout

--- apps/tour-of-heroes-e2e/src/integration/app.spec.ts
+++ apps/tour-of-heroes-e2e/src/integration/app.spec.ts
@@ -1,8 +1,9 @@
-describe('tour-of-heroes', () => {
-  beforeEach(() => cy.visit('/'));
+describe('Dashboard Route Tests', () => {
+  beforeEach(() => cy.visit('/dashboard'));
 
   it('should display h1 and h2 titles', () => {
     cy.get('h1').contains('Welcome to Tour of Heroes!');
-    cy.get('h2').contains('My Heroes');
+    cy.get('h2').contains('Top Heroes');
+    cy.get('h2').contains('Messages');
   });
 });
