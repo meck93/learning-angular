8d0469a33ac3a24c699758e5f798b3f4178466f4
Moritz Eck <moritz.eck@zuehlke.com>
2021-04-13 18:09:42 +0200

    simpler variant to mock activated route

--- apps/tour-of-heroes/src/app/hero-detail/hero-detail.component.spec.ts
+++ apps/tour-of-heroes/src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,54 +1,22 @@
-import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
-import { of } from 'rxjs';
-import { HeroService } from '../hero.service';
-import { Hero } from '../heroes/shared/hero.model';
-import { HEROES } from '../mock-heroes';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
 import { HeroDetailComponent } from './hero-detail.component';
-
-@Component({ template: '<router-outlet></router-outlet>' })
-class TestRootComponent {}
 
 describe('HeroDetailComponent', () => {
   let component: HeroDetailComponent;
   let fixture: ComponentFixture<HeroDetailComponent>;
-  let router: Router;
-  let rootFixture: ComponentFixture<TestRootComponent>;
-  const fakeHeroes: ReadonlyArray<Hero> = [...HEROES];
 
   beforeEach(async () => {
-    const fakeHeroService = {
-      getHero(id: number) {
-        const hero = [...fakeHeroes].find((h) => h.id === id);
-        return of(hero);
-      },
-    } as Partial<HeroService>;
-
     TestBed.configureTestingModule({
-      declarations: [TestRootComponent, HeroDetailComponent],
-      imports: [
-        RouterTestingModule.withRoutes([
-          {
-            path: 'heroes/:id',
-            component: HeroDetailComponent,
-          },
-        ]),
-        FormsModule,
-      ],
+      declarations: [HeroDetailComponent],
       providers: [
         {
-          provide: HeroService,
-          useValue: fakeHeroService,
+          provide: ActivatedRoute,
+          useValue: { snapshot: { paramMap: convertToParamMap({ id: '11' }) } },
         },
       ],
     });
     await TestBed.compileComponents();
-
-    rootFixture = TestBed.createComponent(TestRootComponent);
-    router = TestBed.inject(Router);
 
     fixture = TestBed.createComponent(HeroDetailComponent);
     component = fixture.componentInstance;
@@ -57,8 +25,8 @@ describe('HeroDetailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-
-    // TODO: figure out how to check if the detail component is displayed correctly
-    rootFixture.ngZone?.run(() => router.navigate(['detail', 11]));
+    expect(component.hero).toBeDefined();
+    expect(component.hero?.id).toBe(11);
+    expect(component.hero?.name).toBe('Dr Nice');
   });
 });
