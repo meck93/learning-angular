17691adae45e9357e871ddcbab6ec2012883335a
Moritz Eck <moritz.eck@zuehlke.com>
2021-04-13 10:40:03 +0200

    part 2 of tutorial

--- apps/tour-of-heroes-e2e/src/integration/app.spec.ts
+++ apps/tour-of-heroes-e2e/src/integration/app.spec.ts
@@ -10,6 +10,6 @@ describe('tour-of-heroes', () => {
     // Function helper example, see `../support/app.po.ts` file
     getGreeting().contains('Welcome to Tour of Heroes!');
 
-    cy.get('h2').contains('WINDSTORM Details');
+    cy.get('h2').contains('My Heroes');
   });
 });
