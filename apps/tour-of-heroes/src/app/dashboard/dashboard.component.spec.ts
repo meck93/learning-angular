16a7ddef33f8a656c958ce2353088fbf781de5a1
Moritz Eck <moritz.eck@zuehlke.com>
2021-04-15 16:01:54 +0200

    add routing to tests

--- apps/tour-of-heroes/src/app/dashboard/dashboard.component.spec.ts
+++ apps/tour-of-heroes/src/app/dashboard/dashboard.component.spec.ts
@@ -1,15 +1,29 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { HEROES } from '../../../testing/mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let mockHeroService: Partial<HeroService>;
 
   beforeEach(async () => {
+    mockHeroService = { getHeroes: jest.fn(() => of(HEROES)) };
+
     await TestBed.configureTestingModule({
       declarations: [DashboardComponent],
-      imports: [HttpClientTestingModule],
+      providers: [{ provide: HeroService, useValue: mockHeroService }],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([
+          { path: 'heroes/:id', component: HeroDetailComponent },
+        ]),
+      ],
     }).compileComponents();
   });
 
@@ -21,5 +35,15 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display "Top Heroes" as headline', () => {
+    expect(fixture.nativeElement.querySelector('h2').textContent).toEqual(
+      'Top Heroes'
+    );
+  });
+
+  it('should display 4 links', async () => {
+    expect(fixture.nativeElement.querySelectorAll('a').length).toEqual(4);
   });
 });
