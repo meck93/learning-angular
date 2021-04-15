578ecb4411291669e8713d9edd4f1f635897b838
Moritz Eck <moritz.eck@zuehlke.com>
2021-04-15 15:17:56 +0200

    fix: unit tests

--- apps/tour-of-heroes/src/app/dashboard/dashboard.component.spec.ts
+++ apps/tour-of-heroes/src/app/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,5 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
