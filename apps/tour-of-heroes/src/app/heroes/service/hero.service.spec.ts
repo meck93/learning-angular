8acaa63a96450762af585537880cb5964f77b072
Moritz Eck <moritz.eck@zuehlke.com>
2021-04-16 12:11:52 +0200

    unit test assertions run properly now

--- apps/tour-of-heroes/src/app/heroes/service/hero.service.spec.ts
+++ apps/tour-of-heroes/src/app/heroes/service/hero.service.spec.ts
@@ -1,21 +1,25 @@
-import { HttpClient } from '@angular/common/http';
 import {
   HttpClientTestingModule,
   HttpTestingController,
 } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { MessageService } from '../../messages/service/message.service';
 import { Hero } from '../shared/hero.model';
 import { HeroService } from './hero.service';
 
 describe('HeroService', () => {
+  const mockHeroes: Hero[] = [
+    { id: 11, name: 'Dr Nice' },
+    { id: 12, name: 'Narco' },
+  ];
   let service: HeroService;
   let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [HttpClient, MessageService],
+      providers: [HeroService],
     });
     httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(HeroService);
@@ -29,56 +33,80 @@ describe('HeroService', () => {
     expect(service).toBeTruthy();
   });
 
-  describe('getHeroes', () => {
-    it('should return heroes', () => {
-      // ensure 10 heroes are fetched
-      service.getHeroes().subscribe((heroes) => {
+  it('getHeroes (with Spy)', (done) => {
+    const mockHttpClient = { get: jest.fn(() => of(mockHeroes)) };
+    service = new HeroService(mockHttpClient as any, new MessageService());
+
+    service.getHeroes().subscribe(
+      (heroes) => {
         // TODO: ask @fueg why this test passes even though it clearly receives wrong data
-        expect(heroes.length).toBe(10);
-      }, fail);
+        expect(heroes.length).toBe(2);
+        done(); // this is important -> otherwise the test is useless
+      },
+      (error) => fail(error)
+    );
+    expect(mockHttpClient.get).toHaveBeenCalledTimes(1);
+  });
+
+  describe('getHeroes (with Mock)', () => {
+    it('should return heroes', (done) => {
+      // ensure 2 heroes are fetched
+      service.getHeroes().subscribe(
+        (heroes) => {
+          // TODO: ask @fueg why this test passes even though it clearly receives wrong data
+          expect(heroes.length).toBe(2);
+          done(); // this is important -> otherwise the test is useless
+        },
+        (error) => fail(error)
+      );
 
       // check that a GET request was performed
       const req = httpTestingController.expectOne('api/heroes');
       expect(req.request.method).toEqual('GET');
-      req.flush([]);
+      req.flush(mockHeroes);
     });
 
-    it('should turn 404 into a user-friendly error', () => {
-      service
-        .getHeroes()
-        .subscribe((heroes) => expect(heroes).toEqual([]), fail);
+    it('should turn 404 into a user-friendly error', (done) => {
+      const msg = 'Deliberate 404';
+      service.getHeroes().subscribe(
+        (heroes) => {
+          expect(heroes).toEqual([]);
+          done();
+        },
+        () => fail("shouldn't error")
+      );
 
       const req = httpTestingController.expectOne('api/heroes');
       expect(req.request.method).toEqual('GET');
       // Respond with 404 error
-      req.flush('Invalid request parameters', {
+      req.flush(msg, {
         status: 404,
-        statusText: 'Bad Request',
+        statusText: 'Not Found',
       });
     });
   });
 
   describe('getHero by ID', () => {
-    it('should return hero', () => {
+    it('should return hero', (done) => {
       const id = 11;
       // ensure first hero is fetched
       service.getHero(id).subscribe((hero) => {
-        // TODO: ask @fueg why this test passes even though it clearly receives wrong data
         expect(hero.id).toEqual(id);
         expect(hero.name).toEqual('Dr Nice');
+        done();
       });
 
       // check that a GET request was performed
       const req = httpTestingController.expectOne(`api/heroes/${id}`);
       expect(req.request.method).toEqual('GET');
-      req.flush({ id: 12, name: 'Dr Nice' });
+      req.flush({ id: 11, name: 'Dr Nice' });
     });
 
-    it('should turn 404 into a user-friendly error', () => {
+    it('should turn 404 into a user-friendly error', (done) => {
       const id = 11;
       service.getHero(id).subscribe((response) => {
-        expect(response).toBeNull();
         expect(response).toBeUndefined();
+        done();
       }, fail);
 
       const req = httpTestingController.expectOne(`api/heroes/${id}`);
@@ -93,11 +121,13 @@ describe('HeroService', () => {
   });
 
   describe('addHero', () => {
-    it('should add a single Hero', () => {
+    it('should add a single Hero', (done) => {
       const mockHero = { name: 'Tester' } as Hero;
 
-      service.addHero(mockHero).subscribe((response) => {
-        expect(response).toEqual(mockHero);
+      service.addHero(mockHero).subscribe((createdHero) => {
+        expect(createdHero).toBeDefined();
+        expect(createdHero.name).toEqual(mockHero.name);
+        done();
       }, fail);
 
       // Receive POST request
@@ -108,10 +138,11 @@ describe('HeroService', () => {
       req.flush({ id: 21, name: 'Tester' });
     });
 
-    it('should fail gracefully on error', () => {
+    it('should fail gracefully on error', (done) => {
       const mockHero = (null as unknown) as Hero;
       service.addHero(mockHero).subscribe((response) => {
         expect(response).toBeUndefined();
+        done();
       }, fail);
 
       // Receive POST request
