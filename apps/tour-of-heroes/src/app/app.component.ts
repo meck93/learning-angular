aa77b1f24db5500654dc22ddd126af832301aa4c
Moritz Eck <moritz.eck@zuehlke.com>
2021-04-16 09:33:08 +0200

    move html to inline template

--- apps/tour-of-heroes/src/app/app.component.ts
+++ apps/tour-of-heroes/src/app/app.component.ts
@@ -2,7 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'nx-app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div class="layout">
+      <h1>Welcome to {{ title }}!</h1>
+      <nav>
+        <a routerLink="/dashboard">Dashboard</a>
+        <a routerLink="/heroes">Heroes</a>
+      </nav>
+      <router-outlet></router-outlet>
+      <nx-app-messages></nx-app-messages>
+    </div>
+  `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
