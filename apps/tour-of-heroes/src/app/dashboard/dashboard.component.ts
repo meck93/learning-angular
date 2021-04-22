6c7f271073723a0deae5c9aa1407057ca0441f86
Moritz Eck <moritz.eck@zuehlke.com>
2021-04-22 10:24:27 +0200

    creates a custom observable

--- apps/tour-of-heroes/src/app/dashboard/dashboard.component.ts
+++ apps/tour-of-heroes/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { HeroService } from '../heroes/service/hero.service';
 import { Hero } from '../heroes/shared/hero.model';
 
@@ -16,12 +17,44 @@ import { Hero } from '../heroes/shared/hero.model';
   `,
   styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
   heroes: Hero[] = [];
+
+  private subscription!: Subscription;
 
   constructor(private heroService: HeroService) {}
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
   ngOnInit() {
+    const customObservable = new Observable((subscriber) => {
+      let count = 0;
+      setInterval(() => {
+        subscriber.next(count);
+        if (count === 3) {
+          subscriber.complete();
+        }
+        if (count > 3) {
+          subscriber.error(new Error('Count is greater than 3!'));
+        }
+        count++;
+      }, 1000);
+    });
+
+    this.subscription = customObservable.subscribe(
+      (data) => {
+        console.log('dashboard.component.ts 40 data:', data);
+      },
+      (error) => {
+        console.warn('dashboard.component.ts 45 error:', error);
+      },
+      () => {
+        console.log('dashboard.component.ts 52 completed!');
+      }
+    );
+
     this.getHeroes();
   }
 
