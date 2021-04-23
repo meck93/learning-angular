6cddc57830eb06078c1aed06c11b04d2859827ec
Moritz Eck <moritz.eck@zuehlke.com>
2021-04-23 11:26:26 +0200

    remove browser module from popup lib

--- libs/popup/src/lib/popup.module.ts
+++ libs/popup/src/lib/popup.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PopupComponent } from './popup/popup.component';
 import { PopupService } from './popup/popup.service';
@@ -8,7 +7,7 @@ import { PopupService } from './popup/popup.service';
 // TODO: ask @fueg how and what to export/declare
 // also, see app.module.ts
 @NgModule({
-  imports: [BrowserModule, BrowserAnimationsModule, CommonModule],
+  imports: [BrowserAnimationsModule, CommonModule],
   providers: [PopupService],
   exports: [PopupComponent],
   declarations: [PopupComponent],
